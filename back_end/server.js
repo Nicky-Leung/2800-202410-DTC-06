958f09164f4c5cd5c9a0ecbe369f0ef2e6bba23f
Jarell <mjnvicencio@gmail.com>
2024-05-14 10:59:16 -0700

    move welcome page to ('/')

--- back_end/server.js
+++ back_end/server.js
@@ -6,11 +6,15 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
+    res.render('welcomepage')
+});// put welcome page here later
 
+app.get('/index', (req, res) => {
     res.render('index')
-});
+}
+);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
-});
+}); // put later in main func with db connection
 
