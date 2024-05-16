305e70e0c8bf4c5d2ee3435517c545bf600ebeac
persephone-wong <persephone.w315@gmail.com>
2024-05-16 15:31:31 -0700

    add score tracking to match page

--- back_end/public/scripts/match.js
+++ back_end/public/scripts/match.js
@@ -20,3 +20,30 @@ function updateCountdown() {
     time--;
 }
 
+const homeScoreElement = document.getElementById('home-score');
+const awayScoreElement = document.getElementById('away-score');
+const homePlus = document.getElementById('homePlus');
+const homeMinus = document.getElementById('homeMinus');
+const awayPlus = document.getElementById('awayPlus');
+const awayMinus = document.getElementById('awayMinus');
+
+homePlus.addEventListener('click', () => {
+    HomeScore++;
+    homeScoreElement.innerHTML = HomeScore;
+});
+homeMinus.addEventListener('click', () => {
+    if (HomeScore > 0) {
+    HomeScore--;
+    homeScoreElement.innerHTML = HomeScore;}
+});
+awayPlus.addEventListener('click', () => {
+    AwayScore++;
+    awayScoreElement.innerHTML = AwayScore;
+});
+awayMinus.addEventListener('click', () => {
+    if (AwayScore > 0) {
+    AwayScore--;
+    awayScoreElement.innerHTML = AwayScore;}
+});
+
+homeScoreElement.innerHTML = HomeScore;
