90aec1bb836da97eafa69dcbe53da898dde0f2f7
persephone-wong <persephone.w315@gmail.com>
2024-05-16 10:07:06 -0700

    add eventlisteners for buttons

---
+++ back_end/public/scripts/leaderboard.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById("backButton").addEventListener("click", goBack);
+
+document.getElementById("localBoard").addEventListener("click", goToLocal);
+
+document.getElementById("globalBoard").addEventListener("click", goToGlobal);
+
+document.getElementById("regionalBoard").addEventListener("click", goToRegional);
+
