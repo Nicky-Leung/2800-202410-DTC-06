35a1707ae524e404178d2f16d3b1da7db32df368
Nicky Leung <nicky-leung@outlook.com>
2024-05-16 11:35:46 -0700

    Add opacity and blur to information and footer elements

--- back_end/tailwind.config.js
+++ back_end/tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ['views/**/*.{html,ejs,js}'],
   theme: {
     extend: {
+      backdropBlur: {
+        'none': '0',
+        'xs': '2px',
+        'sm': '4px',
+        'DEFAULT': '8px',
+        'md': '12px',
+        'lg': '16px',
+        'xl': '24px',
+        '2xl': '40px',
+        '3xl': '64px',
+      },
+      
       colors: {
         'crimson': '#D62839',
         'rose': '#BA324F',
