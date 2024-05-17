2e534cbe24bde38156fa989bebf8b72bbd4d80ba
Nicky Leung <nicky-leung@outlook.com>
2024-05-16 22:54:19 -0700

    change map style

--- back_end/routes/map.js
+++ back_end/routes/map.js
@@ -5,7 +5,8 @@ const fetch = require('node-fetch');
 
 router.get('/map-data', async (req, res) => {
   try {
-    const response = await fetch(`https://api.maptiler.com/maps/dataviz/style.json?key=${process.env.MapKey}`);
+    const response = await fetch(`https://api.maptiler.com/maps/75069241-05c3-417f-914f-a7ef07072f3d/style.json?key=${process.env.MapKey}`);
+
 
     const data = await response.json();
     res.json(data);
