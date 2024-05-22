1acff135c1239e49412388b988d5c61730b0834d
Jarell <mjnvicencio@gmail.com>
2024-05-22 04:24:41 -0700

    import cloudinary and multer for pfp upload. Store uploaded pfp as string in db for user

--- back_end/routes/updateProfile.js
+++ back_end/routes/updateProfile.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const usersModel = require('../models/userModel');
+const cloudinary = require('../public/scripts/cloudinary')
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-router.post('/updateProfile', async (req, res) => {
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'profilePicturesforCOMP2800',// folder where teh pfp is stored
+    allowed_formats: ['jpg', 'png', 'jpeg', 'gif']// allowed pfp formats
+
+  },
+});
+
+const parser = multer({ storage: storage });// middleware for parsing the pfp
+
+
+router.post('/updateProfile', parser.single('profilePicture'), async (req, res) => {
   const { name, bio } = req.body;
   const update = { name, bio };
+
+  if (req.file) {
+    update.profilePicture = req.file.path;// updates pfp 
+  }
+
   try {
     await usersModel.updateOne({ email: req.session.email }, update);
     // Update session data
     req.session.name = name;
     req.session.bio = bio;
+    if (req.file) {
+      req.session.profilePicture = req.file.path;// updates pfp in session
+    }
     res.redirect('/profile?profileUpdated=true');
   } catch (error) {
     console.error('Error during profile update:', error);
