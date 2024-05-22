ce5c17e37aa07fdd9d1926a8f0812348bb1473cc
Nicky Leung <nicky-leung@outlook.com>
2024-05-22 14:10:46 -0700

    Add dynamic cards for each location

--- back_end/public/scripts/index.js
+++ back_end/public/scripts/index.js
@@ -23,15 +23,22 @@ async function createMap() {
     let features = [];
 
     for (let i = 0; i < sessions.length; i++) {
+        let time = new Date(sessions[i].time);
         features.push({
             'type': 'Feature',
             'properties': {
-                'icon': 'basketball'
+                'id': sessions[i]._id,
+                'location': sessions[i].location.name,
+                'matchType': sessions[i].matchType,
+                'time': `${time.getHours()}:${time.getMinutes()}`,
+                'date': sessions[i].date,
+                'icon': sessions[i].sport 
             },
             'geometry': {
                 'type': 'Point',
                 'coordinates': [sessions[i].location.coordinates[0], sessions[i].location.coordinates[1]]
-            }
+            },
+
         });
 
     }
@@ -39,12 +46,12 @@ async function createMap() {
     map.on('load', () => {
 
         map.loadImage(
-            'basketball.jpg',
+            'images/basketball.jpg',
             function (error, image) {
-              if (error) throw error;
-              map.addImage('basketball', image);
+                if (error) throw error;
+                map.addImage('basketball', image);
             }
-          );
+        );
         map.addSource('places', {
             'type': 'geojson',
             'data': {
@@ -58,39 +65,52 @@ async function createMap() {
             'type': 'symbol',
             'source': 'places',
             'layout': {
-                'icon-image':'{icon}' ,
+                'icon-image': `{icon}`,
                 'icon-size': 2,
                 'icon-overlap': 'always'
             }
         });
 
-       // When a click event occurs on a feature in the places layer
-       //opens a UI element populated by /information
+        // When a click event occurs on a feature in the places layer
+        //opens a UI element populated by /information
         map.on('click', 'places', (event) => {
 
-            
 
-            fetch('/information')
-            .then(response => response.text())
-            .then(html => {
-              const element = document.getElementById('overlay');
-              //clear the html
-              element.innerHTML = "";
-              //add the new html
-              element.innerHTML = html;
-            });
+            console.log(event.features[0].properties.id);
+            fetch('/information',{
+                method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        matchType: event.features[0].properties.matchType,
+                        matchTime: event.features[0].properties.time,
+                        matchDate: event.features[0].properties.date,
+                        matchLocation: event.features[0].properties.location,
+                        matchID: event.features[0].properties.id
+                    })
+            })
+                .then(response => response.text())
+                .then(html => {
+                    const element = document.getElementById('overlay');
+                    //clear the html
+                    element.innerHTML = "";
+                    //add the new html
+                    element.innerHTML = html;
+                });
+            
 
             map.flyTo({
                 center: event.lngLat,
                 essential: true
-            
-                
-              });
-                
-          
+
+
+            });
+
+
         });
 
-       
+
     });
 
 
