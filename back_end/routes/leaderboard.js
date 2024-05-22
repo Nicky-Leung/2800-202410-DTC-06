09c9de2e8141073061194867fe0feb4e3653b032
persephone-wong <persephone.w315@gmail.com>
2024-05-22 14:37:42 -0700

    add dynamic addition of users in leaderboard

--- back_end/routes/leaderboard.js
+++ back_end/routes/leaderboard.js
@@ -6,8 +6,9 @@ const usersModel = require('../models/userModel');
 router.get('/leaderboard', async (req, res) => {
     try {
         const topusers = await usersModel.find({}, {name: 1, rank: 1, elo: 1, profilePicture: 1}).sort({elo: -1}).limit(3);
-        console.log(topusers);
-        res.render('leaderboard.ejs',{topusers: topusers});
+        const allusers = await usersModel.find({}, {name: 1, rank: 1, elo: 1, profilePicture: 1}).sort({elo: -1});
+        console.log(allusers);
+        res.render('leaderboard.ejs',{topusers: topusers, allusers: allusers});
       } catch (err) {
         console.log('error fetching users');
       }
