2ac2842b5c72667097af10d59f87c63d54a4e158
Nicky Leung <nicky-leung@outlook.com>
2024-05-23 17:53:34 -0700

    implement places api to create match page

--- back_end/public/scripts/creatematch.js
+++ back_end/public/scripts/creatematch.js
@@ -1,11 +1,61 @@
-async function getPlaces() {
-    const response = await fetch('/placeSearch');
+async function getPlaces(e) {
+    e.preventDefault();
+    const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+
+    const sport = document.getElementById('sport').value;
+    const radius = document.getElementById('radius').value;
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    response = await fetch('/placeSearch', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            sport,
+            radius,
+            lat,
+            lng
+        })
+    });
+    const data = await response.json();
+    populateLocation(data, sport);
+
+    
 
 
 }
 
+findplace = document.getElementById('findPlace');
+console.log(findplace);
+findplace.addEventListener('click', getPlaces);
 
-async function getvalue() {
+async function populateLocation(data, sport) {
+
+    
+    
+    console.log(data);
+    matchForm = document.getElementById('matchSetUp');
+    matchForm.classList.remove('hidden');
+
+    locationForm = document.getElementById('location');
+
+    Array.from(data.results).forEach((place, index) => {
+        const option = document.createElement('option');
+        option.value = JSON.stringify([place.geocodes.main.longitude, place.geocodes.main.latitude, place.name]);
+        option.textContent = place.name;
+        locationForm.appendChild(option);
+
+    });
+
+    const hiddenInput = document.createElement('input');
+    hiddenInput.type = 'hidden';
+    hiddenInput.name = "sport";
+    hiddenInput.value = sport;
+    hiddenInput.id = "sport";
+    matchForm.insertBefore(hiddenInput, matchForm.firstChild);
 
 
 
--- back_end/routes/placeSearch.js
+++ back_end/routes/placeSearch.js
@@ -2,11 +2,20 @@ const sdk = require('@api/fsq-developers');
 const express = require('express');
 const router = express.Router();
 
-router.get('/placeSearch', (req, res) => {
+router.post('/placeSearch', async (req, res) => {
+
+    query = req.body.sport;
+  
+    radius = req.body.radius;
+    lat = req.body.lat;
+    lng = req.body.lng;
     
     sdk.auth(`${process.env.Foursquare_API_Key}`)
-    sdk.placeSearch({query: `${req.query.query} Court`, ll: `${req.query.lat},${req.query.lng}`})
-    .then(({ data }) => res.json(data))
+    sdk.placeSearch({query: `${query} Court`, ll: `${lat},${lng}`,radius: `${radius}`})
+    .then(({ data }) => {
+        console.log(data);
+        res.json(data);
+    })
     .catch(err => res.status(500).json({ error: err.message }));
 
 });
