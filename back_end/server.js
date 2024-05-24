47f4fb23928cdb1443a7f00f764fa9e56c24203c
NICK_THUCYDIDES <nicholas.thucydides@gmail.com>
2024-05-23 22:24:04 -0700

    Reorganize server.js

--- back_end/server.js
+++ back_end/server.js
@@ -1,19 +1,19 @@
-//Please put routes in routes file instead of server.js so that the code is more organized and easier to read.
+// Please put routes in routes file instead of server.js so that the code is more organized and easier to read.
 const express = require('express')
 const app = express()
 const path = require('path')
-app.set('view engine', 'ejs')
+const mongoose = require('mongoose');
 var session = require('express-session');
-require('dotenv').config();
 var mongoDBStore = require('connect-mongodb-session')(session);
+require('dotenv').config();
 
+app.set('view engine', 'ejs')
 
+// MongoDB connection
 var store = new mongoDBStore({
   uri: `mongodb+srv://${process.env.DB_User}:${process.env.DB_Password}@cluster0.9kdinuu.mongodb.net/connect_mongodb_session_test?retryWrites=true&w=majority`,
   collection: 'mySessions'
 });
-
-const mongoose = require('mongoose');
 
 async function main() {
   try {
@@ -30,6 +30,7 @@ main().catch(err => console.log(err));
 const usersModel = require('./models/userModel');
 const matchModel = require('./models/matchModel');
 
+// Session setup
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
@@ -42,47 +43,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
-// home page
-app.get('/', (req, res) => {
-
-  res.render('home.ejs', { passwordUpdated: req.query.passwordUpdated })
-})
-app.get('/home', (req, res) => {
-  res.render('welcomepage.ejs')
-});
-
-const login = require('./routes/login');
-app.use(login);
-
-const placeSearch = require('./routes/placeSearch');
-app.use(placeSearch);
-
-
-
-
-isUserAuthenticated = (req, res, next) => {
-  if (req.session.authenticated)
-    next()
-  else
-    res.status(401).render('notLoggedIn.ejs', { message: 'Please login first' })
-};
-
-app.use(isUserAuthenticated);
-
-const indexscript = require('./routes/indexscript');
-app.use(indexscript);
-// put welcome page here later
-
-app.get('/index', (req, res) => {
-  res.render('index')
-}
-);
-
-
-
-// check if a user is logged in
-// user must be logged in to access the following routes
+// Middleware to check if user is logged in 
 isUserAuthenticated = (req, res, next) => {
   if (req.session.authenticated) {
     req.currentUser = req.session.currentUser;
@@ -93,88 +54,76 @@ isUserAuthenticated = (req, res, next) => {
 };
 
 
+// Public Routes
+app.get('/', (req, res) => {
+  res.render('home.ejs', { passwordUpdated: req.query.passwordUpdated })
+})
+app.get('/home', (req, res) => {
+  res.render('welcomepage.ejs')
+});
 
-//leaderboard pages
-
+const login = require('./routes/login');
+const placeSearch = require('./routes/placeSearch');
+const indexscript = require('./routes/indexscript');
 const leaderboard = require('./routes/leaderboard');
+const information = require('./routes/information');
+const addFriends = require('./routes/addFriends')
+const resetPassword = require('./routes/resetPassword');
+const resetPasswordWithEmail = require('./routes/resetPasswordWithEmail');
+const profile = require('./routes/profile');
+const otherProfile = require('./routes/otherProfile');
+const map = require('./routes/map');
+const lobby = require('./routes/lobby');
+const matchSessions = require('./routes/matchSessions');
+const searchFriends = require('./routes/searchFriends')
+const createMatch = require('./routes/createMatch');
+const updateProfile = require('./routes/updateProfile');
+const settings = require('./routes/settings');
+
+app.use(login);
+app.use(information);
+app.use(resetPasswordWithEmail);
+app.use(isUserAuthenticated);
+
+// Protected Routes (User must be logged in)
+app.use(indexscript);
+app.use(placeSearch);
+app.use(resetPassword);
+app.use(settings);
+app.use(addFriends)
+app.use(profile);
+app.use(otherProfile);
+app.use(map);
+app.use(updateProfile);
+app.use(createMatch);
+app.use(searchFriends)
+app.use(matchSessions);
+app.use(lobby);
+
+
+app.get('/index', (req, res) => {
+  res.render('index')
+}
+);
 app.get('/localleaderboard', leaderboard);
 app.get('/regionalleaderboard', leaderboard);
 app.get('/globalleaderboard', leaderboard);
-
 app.get('/match', (req, res) => {
   res.render('match.ejs')
 });
-
 app.get('/matchend', (req, res) => {
   res.render('matchover.ejs')
 });
-
-const information = require('./routes/information');
-app.use(information);
-
-const addFriends = require('./routes/addFriends')
-app.use(addFriends)
-
-// user profile
-const profile = require('./routes/profile');
-app.use(profile);
-
-//other user's profile
-const otherProfile = require('./routes/otherProfile');
-app.use(otherProfile);
-
-const map = require('./routes/map');
-app.use(map);
-
-const settings = require('./routes/settings');
-app.use(settings);
 
 app.get('/logout', isUserAuthenticated, function (req, res) {
   req.session.destroy(function (err) {
     res.redirect('/');
   });
 });
-
-// page where user can edit profile directly, except email and password
 app.get('/editProfile', isUserAuthenticated, (req, res) => {
   res.render('editProfile.ejs', { name: req.session.name, email: req.session.email, type: req.session.type, bio: req.session.bio, profilePicture: req.session.profilePicture })
 });
 
-const resetPassword = require('./routes/resetPassword');
-const resetPasswordWithEmail = require('./routes/resetPasswordWithEmail');
-
-app.use(resetPassword);
-app.use(resetPasswordWithEmail);
-
-const updateProfile = require('./routes/updateProfile');
-app.use(updateProfile);
-
-const createMatch = require('./routes/createMatch');
-app.use(createMatch);
-
-
-// app.get('/searchFriends', isUserAuthenticated, async (req, res) => {
-//   const query = req.query.query;
-//   let friends = [];
-//   if (query) {
-//     try {
-//       friends = await usersModel.find({ name: new RegExp(query, 'i') }).exec();
-//     } catch (error) {
-//       console.error('Error searching for friends:', error);
-//     }
-//   }
-//   res.render('searchFriends.ejs', { friends });
-// });
-
-const searchFriends = require('./routes/searchFriends')
-app.use(searchFriends)
-
-const matchSessions = require('./routes/matchSessions');
-app.use(matchSessions);
-
-const lobby = require('./routes/lobby');
-app.use(lobby);
-
 app.listen(3000, () => {
-  console.log(`Server running on port 3000!!!`)
+  console.log(`Server running on port 3000!`)
 })  
