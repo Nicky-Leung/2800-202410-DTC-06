45ab3710ac03911e4b90a323fdb26dc735436d7a
Nicky Leung <nicky-leung@outlook.com>
2024-05-26 21:50:27 -0700

    add cards to creatematch page

--- back_end/tailwind.config.js
+++ back_end/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['views/**/*.{html,ejs,js}'],
+  content: ['views/**/*.{html,ejs,js}', 'public/**/*.js'],
   theme: {
     extend: {
       backdropBlur: {
