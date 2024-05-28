23938b472eab871c876d99b44e7c2aed51b44050
Jarell <mjnvicencio@gmail.com>
2024-05-28 13:31:11 -0700

    add post route that updates user's elo based on match result

--- back_end/routes/matchover.js
+++ back_end/routes/matchover.js
@@ -57,13 +57,65 @@ router.get('/matchend', async (req, res) => {
             homescore: currentMatch.score.home,
             awayscore: currentMatch.score.away,
             currentUser: currentUser,
-            currentuserid: req.session.currentUser ? req.session.currentUser._id : null,
+            currentuserid: req.session.currentUser ? req.session.currentUser._id : null, // sending current user id to front end for ejs
             tie: tie
         });
+
     } catch (error) {
         console.error('Error fetching match data:', error);
         res.status(500).json({ message: "Internal Server Error" });
     }
 });
+
+router.post('/updateElo', async (req, res) => {
+    try {
+        let matchID;
+        if (req.session.activematch) {
+            matchID = req.session.activematch;
+        } else if (req.query.matchID) {
+            matchID = req.query.matchID;
+            req.session.activematch = matchID;
+        } else {
+            return res.status(400).json({ message: "No match ID!" });
+        }
+
+        const currentMatch = await matchModel.findOne({ _id: matchID });
+        if (!currentMatch) {
+            return res.status(404).json({ message: "Match not found" });
+        }
+        const currentUser = req.session.currentUser;
+        let currentTeam;
+        if (currentMatch.homePlayers.some(player => player._id.equals(currentUser._id))) {// if user is in home team
+            currentTeam = currentMatch.homePlayers;
+        } else if (currentMatch.awayPlayers.some(player => player._id.equals(currentUser._id))) {// if user is in away team
+            currentTeam = currentMatch.awayPlayers;
+        } else {
+            return res.status(400).json({ message: "Current user not found in match teams" });// catch error if user is in no team
+        }
+
+        let winningTeam;
+        if (currentMatch.score.home > currentMatch.score.away) {// home wins
+            winningTeam = currentMatch.homePlayers;
+        } else if (currentMatch.score.home < currentMatch.score.away) {// away wins
+            winningTeam = currentMatch.awayPlayers;
+        } else {
+            winningTeam = null; // tie
+        }
+        const elochange = winningTeam && winningTeam.some(player => player._id.equals(currentUser._id)) ? 20 : -20 // elo change
+
+
+        await Promise.all(currentTeam.map(async (player) => {
+            const user = await usersModel.findById(player._id);
+            user.elo += elochange;
+            await user.save();
+        }
+        ));
+        res.status(200).json({ message: "Elo updated" });
+    } catch (error) {
+        console.error('Error updating elo:', error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+
+})
 
 module.exports = router;
