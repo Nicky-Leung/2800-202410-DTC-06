0bdb21b0be27e5962b54ab2371a4ad4a8601a214
NICK_THUCYDIDES <nicholas.thucydides@gmail.com>
2024-05-29 12:03:48 -0700

    Fix add fried

--- back_end/routes/otherProfile.js
+++ back_end/routes/otherProfile.js
@@ -9,15 +9,36 @@ router.use('/public', express.static('public'));
 router.get('/otherProfile/:userid', async (req, res) => {
     try {
         const user = await usermodel.findOne({ _id: req.params.userid });
+        console.log('OP : user: ', user)
         if (!user) {
             return res.status(404).send('User not found');
         }
 
         const current = req.currentUser;
+        console.log('OP : current:  ', current)
+        console.log('current id: ', current._id)
+        current2 = await usermodel.findOne({ _id: current._id });
+        console.log('current user:', current)
+
         let isFriend = false;
-        if (current && current.friends.map(friend => friend.toString()).includes(req.params.userid)) {
+        console.log('user id:', req.params.userid)
+        if (current2 && current2.friends.map(friend => friend.toString()).includes(req.params.userid)) {
             isFriend = true;
+            console.log('you are friends')
+        } else {
+            console.log('you are not friends')
         }
+
+        // console.log('user id:', req.params.userid)
+        // if (ohio && ohio.friends.map(friend => friend.toString()).includes(req.params.userid)) {
+        //     isFriend = true;
+        //     console.log('you are friends')
+        // } else {
+
+        //     console.log('you are not friends')
+        // }
+
+
 
         res.render('otherProfile.ejs', { name: user.name, email: user.email, type: user.type, bio: user.bio, profilePicture: user.profilePicture, isFriend, rank: user.rank, sportsmanship: user.sportsmanship, elo: user.elo, streak: user.streak, streakCount: user.streakCount, matchHistory: user.matchHistory });
     } catch (err) {
