6ac2c3c7614e0bf4a5f269208470b34a54a9a29e
Jarell <mjnvicencio@gmail.com>
2024-05-29 11:35:50 -0700

    fix typo. Fix user not being able to rate the same player across different matches

--- back_end/public/scripts/rateplayer.js
+++ back_end/public/scripts/rateplayer.js
@@ -1,7 +1,6 @@
 // Get the rating dialog element
 var ratingDialog = document.getElementById("ratingDialog");
 
-
 // Get the close button element
 var closeBtn = document.getElementById("closeBtn");
 
@@ -12,16 +11,15 @@ var ratedUsers = JSON.parse(localStorage.getItem('ratedUsers')) || [];
 icons.innerHTML = `
 <div class="terrible" id="terrible" style="display: flex; flex-direction: column; align-items: center; justify-content: center;"> <!-- -elo -->
     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mood-cry" width="75" height="75" viewBox="0 0 24 24" stroke-width="1" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
-  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <path d="M9 10l.01 0" />
-  <path d="M15 10l.01 0" />
-  <path d="M9.5 15.25a3.5 3.5 0 0 1 5 0" />
-  <path d="M17.566 17.606a2 2 0 1 0 2.897 .03l-1.463 -1.636l-1.434 1.606z" />
-  <path d="M20.865 13.517a8.937 8.937 0 0 0 .135 -1.517a9 9 0 1 0 -9 9c.69 0 1.36 -.076 2 -.222" />
-</svg>
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M9 10l.01 0" />
+        <path d="M15 10l.01 0" />
+        <path d="M9.5 15.25a3.5 3.5 0 0 1 5 0" />
+        <path d="M17.566 17.606a2 2 0 1 0 2.897 .03l-1.463 -1.636l-1.434 1.606z" />
+        <path d="M20.865 13.517a8.937 8.937 0 0 0 .135 -1.517a9 9 0 1 0 -9 9c.69 0 1.36 -.076 2 -.222" />
+    </svg>
     <span style="margin-top: 8px;">Terrible</span>
 </div>
-
 
 <div class="neutral" id="neutral" style="display: flex; flex-direction: column; align-items: center; justify-content: center;"> <!-- No changes-->
     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mood-empty" width="75" height="75" viewBox="0 0 24 24" stroke-width="1" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -33,8 +31,6 @@ icons.innerHTML = `
     </svg>
     <span style="margin-top: 8px;">Neutral</span>
 </div>
-
-
 
 <div class="happy" id="happy" style="display: flex; flex-direction: column; align-items: center; justify-content: center;"> <!-- +elo -->
     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mood-happy" width="75" height="75" viewBox="0 0 24 24" stroke-width="1" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -48,7 +44,6 @@ icons.innerHTML = `
 </div>
 `;
 
-
 // Add event listener to close the modal when the close button is clicked
 closeBtn.addEventListener("click", function () {
     ratingDialog.classList.add("hidden");
@@ -60,18 +55,19 @@ window.addEventListener("click", function (event) {
         ratingDialog.classList.add("hidden");
     }
 });
+
 // Get all flag icons
 var flagIcons = document.querySelectorAll(".flagIcon");
 
 // Add event listener to each flag icon to open the rating dialog
 flagIcons.forEach(function (icon) {
-    icon.addEventListener("click", function () {
+    icon.addEventListener("click", function (event) {
         var playerId = event.currentTarget.closest("[data-player-id]").dataset.playerId; // fetches id
-        const currentUserId = document.querySelector('[data-current-user-id]').dataset.currentUserId;// fetches current user id
+        var currentUserId = document.querySelector('[data-current-user-id]').dataset.currentUserId; // fetches current user id
         var matchId = event.currentTarget.closest("[data-match-id]").dataset.matchId;
 
         console.log("Flag icon clicked");
-        // console.log("Player ID:", playerId);
+        console.log("Player ID:", playerId);
         console.log("Current user ID:", currentUserId);
 
         if (playerId === currentUserId) {
@@ -104,9 +100,6 @@ function hasRatedUser(playerId, matchId) {
     return ratedUsers.some(rating => rating.playerId === playerId && rating.matchId === matchId);
 }
 
-
-
-
 // Add event listener to each rating icon
 faceIcons.forEach(function (icon) {
     icon.addEventListener("click", function () {
@@ -123,14 +116,13 @@ faceIcons.forEach(function (icon) {
 });
 
 submit.addEventListener("click", async function () {
-
     // Check if a rating is picked
     if (!ratingpicked) {
         alert("Please select a rating before submitting");
         return;
     }
 
-    var playerId = ratingDialog.getAttribute("data-player-id");// access the id via attr
+    var playerId = ratingDialog.getAttribute("data-player-id"); // access the id via attr
     var matchId = ratingDialog.getAttribute("data-match-id"); // access the match id via attr
 
     if (hasRatedUser(playerId, matchId)) {
@@ -138,9 +130,8 @@ submit.addEventListener("click", async function () {
         return;
     }
 
-
     try {
-        const resp = await fetch('/updateSportsmanship', {// this post is in matchover.js
+        const resp = await fetch('/updateSportsmanship', { // this post is in matchover.js
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -154,12 +145,8 @@ submit.addEventListener("click", async function () {
 
         alert(data.message);
         ratingDialog.classList.add("hidden");
-
-
-    }
-    catch (err) {
+    } catch (err) {
         console.error('Error updating sportsmanship:', err);
         alert('Error updating sportsmanship');
     }
 });
-
