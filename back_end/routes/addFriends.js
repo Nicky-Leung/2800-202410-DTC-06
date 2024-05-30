f370da0fb776d45cd003a7986a746cc91ab2a1aa
NICK_THUCYDIDES <nicholas.thucydides@gmail.com>
2024-05-29 18:54:44 -0700

    Fix add/remove friend buttons

--- back_end/routes/addFriends.js
+++ back_end/routes/addFriends.js
@@ -13,6 +13,7 @@ router.get('/addFriends', isUserAuthenticated, async (req, res) => {
   }
 });
 
+// Add a user to friends
 router.post('/addFriends/befriend', isUserAuthenticated, async (req, res) => {
   console.log('user:', req.currentUser)
   console.log('friendID:', req.body.friendId)
@@ -30,13 +31,39 @@ router.post('/addFriends/befriend', isUserAuthenticated, async (req, res) => {
 
     await usersModel.findByIdAndUpdate(currentUser._id, { $addToSet: { friends: friendId } });
     await usersModel.findByIdAndUpdate(friendId, { $addToSet: { friends: currentUser._id } });
+    res.redirect(`/otherprofile/${friendId}`);
 
-    res.redirect('/addFriends');
+
   } catch (err) {
     console.error('Error befriending user:', err);
     res.status(500).send('Error befriending user');
   }
 });
 
+// Remove a user from friends
+router.post('/addFriends/unfriend', isUserAuthenticated, async (req, res) => {
+  try {
+    const currentUser = req.currentUser;
+    const friendId = req.body.friendId;
+
+    if (!currentUser || !currentUser._id) {
+      throw new Error('Current user is not available or does not have an ID.');
+    }
+
+    if (!friendId) {
+      throw new Error('Friend ID is not provided in the request body.');
+    }
+
+    await usersModel.findByIdAndUpdate(currentUser._id, { $pull: { friends: friendId } });
+    await usersModel.findByIdAndUpdate(friendId, { $pull: { friends: currentUser._id } });
+
+    // Redirect back to  previous page
+    res.redirect('back');
+
+  } catch (err) {
+    console.error('Error unfriending user:', err);
+    res.status(500).send('Error unfriending user');
+  }
+});
 
 module.exports = router;
